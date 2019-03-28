39724473a849ec48a18422270ddcc3c9b1020c84
jungwonalicia <aliciawill@naver.com>
2019-03-28 21:52:34 +0900

    Add files via upload

---
+++ python01/start/hello.py
@@ -0,0 +1,24 @@
+#basic output : monitor출력
+print("hi.....");
+print('안녕하세요.');
+
+#basic input : keyboard
+name = input("your name: ")
+year = input("your birth year: ")
+
+print("your input name is ", name)
+print("your input year is ", year)
+
+yearInt = int(year)
+
+age = 2019 - yearInt + 1
+print("your age is ", age)
+
+
+
+
+
+
+
+
+
---
+++ python01/start/hi.py
@@ -0,0 +1,6 @@
+'''
+Created on 2019. 3. 28.
+
+@author: user
+'''
+print("hi")
